8283172ff10c12796ba505ef32fd54fd32be9d71
Mayank <innovativecreations195@gmail.com>
2025-01-31 15:20:47 +0530

    set alarm

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import tkinter as tk
 from time import strftime
 from tkinter import ttk
+import datetime
 
 def update_time():
     current_time = strftime("%H:%M:%S")
     time_label.config(text=current_time)
     # stroke.config(text=current_time)
+    if current_time == alarm_entry.get():
+        ring_bell()
     root.after(1000, update_time)
+
+def ring_bell():
+    print("It's time")
+
+def set_alarm():
+    # date_format = '%Y-%m-%d %H:%M:%S'
+    
+    alarm_time = alarm_entry.get()
+    time_data = alarm_time.split(":")
+    if len(time_data) == 3:
+        print("alarm has been set")
+    else:
+        print("Check data")    
+
+    print(alarm_time)
 
 root = tk.Tk()
 root.title("Real-Time Clock")
@@ -26,8 +44,6 @@ tab1 = tk.Frame(notebook, bg="#3b3b3b")
 notebook.add(tab1, text="Clock")
 
 
-
-
 # stroke = tk.Label(root, font=("Helvetica", 60), fg="black", bg="#3b3b3b")
 # # stroke.place(relx=0.5, rely=0.5, anchor="center")
 # stroke.place(relx=0.5, rely=0.5, anchor="center", x=30, y=30)
@@ -39,8 +55,16 @@ time_label.place(relx=0.5, rely=0.5, anchor="center")
 tab2 = tk.Frame(notebook, bg="#3b3b3b")
 notebook.add(tab2, text="Alarm")
 
-label_tab2 = tk.Label(tab2, text="Tab2", font=("Helvetica", 20), fg="#f0f0f0", bg="#3b3b3b")
-label_tab2.place(relx=0.5, rely=0.5, anchor="center")
+label_tab2 = tk.Label(tab2, text="Set an Alarm:", font=("Helvetica", 20), fg="#f0f0f0", bg="#3b3b3b")
+label_tab2.place(relx=0.5, rely=0.3, anchor="center")
+
+alarm_time = tk.StringVar()
+alarm_entry = tk.Entry(tab2, textvariable=alarm_time, font=("Helvetica", 14), width=10)
+alarm_entry.place(relx=0.5, rely=0.4, anchor="center")
+
+set_alarm_button = tk.Button(tab2, text="Set Alarm (Format: HH:MM:SS)", font=("Helvetica", 14), command=set_alarm)
+set_alarm_button.place(relx=0.5, rely=0.5, anchor="center")
+
 
 # time_label.pack()
 update_time()
--- trying_ui.py
+++ trying_ui.py
@@ -1,53 +1,60 @@
-# import tkinter as tk
-# import datetime
+# # import tkinter as tk
+# # import datetime
 
-# currentTime = datetime.datetime.now().time().strftime("%H %M")
+# # currentTime = datetime.datetime.now().time().strftime("%H %M")
 
-# def setTime():
-#     text_var.set(currentTime)
+# # def setTime():
+# #     text_var.set(currentTime)
 
-# root = tk.Tk()
+# # root = tk.Tk()
 
-# text_var = tk.StringVar()
+# # text_var = tk.StringVar()
 
-# text_var.set("Old Text")
+# # text_var.set("Old Text")
 
-# root.configure(background="#000000")
-# w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-
-
-# label = tk.Label(root, textvariable=text_var)
-# label.grid(column=0, row=0)
+# # root.configure(background="#000000")
+# # w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 
 
-# quit_button = tk.Button(root, text="Quit", command=root.destroy)
-# quit_button.grid(column=0, row=1)
+# # label = tk.Label(root, textvariable=text_var)
+# # label.grid(column=0, row=0)
 
 
-# change_button = tk.Button(root, text="Change", command=setTime)
-# change_button.grid(column=0, row=2)
+# # quit_button = tk.Button(root, text="Quit", command=root.destroy)
+# # quit_button.grid(column=0, row=1)
 
 
-# root.mainloop()
+# # change_button = tk.Button(root, text="Change", command=setTime)
+# # change_button.grid(column=0, row=2)
+
+
+# # root.mainloop()
 
 
 
-import tkinter as tk					 
-from tkinter import ttk 
+# import tkinter as tk					 
+# from tkinter import ttk 
 
 
-root = tk.Tk() 
-root.title("Tab Widget") 
-tabControl = ttk.Notebook(root) 
+# root = tk.Tk() 
+# root.title("Tab Widget") 
+# tabControl = ttk.Notebook(root) 
 
-tab1 = ttk.Frame(tabControl) 
-tab2 = ttk.Frame(tabControl) 
+# tab1 = ttk.Frame(tabControl) 
+# tab2 = ttk.Frame(tabControl) 
 
-tabControl.add(tab1, text ='Tab 1') 
-tabControl.add(tab2, text ='Tab 2') 
-tabControl.pack(expand = 1, fill ="both") 
+# tabControl.add(tab1, text ='Tab 1') 
+# tabControl.add(tab2, text ='Tab 2') 
+# tabControl.pack(expand = 1, fill ="both") 
 
-ttk.Label(tab1, text ="tab 1").grid(column = 0, row = 0, padx = 30, pady = 30) 
-ttk.Label(tab2, text ="tab 2").grid(column = 0, row = 0, padx = 30, pady = 30) 
+# ttk.Label(tab1, text ="tab 1").grid(column = 0, row = 0, padx = 30, pady = 30) 
+# ttk.Label(tab2, text ="tab 2").grid(column = 0, row = 0, padx = 30, pady = 30) 
 
-root.mainloop() 
+# root.mainloop() 
+
+from datetime import datetime
+
+date_str = '14:30:00'
+date_format = '%H:%M:%S'
+date_obj = datetime.strptime(date_str, date_format)
+print(date_obj)
